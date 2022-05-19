fc5a052d383a1f6a66031bf07448ac4e7f9d4307
Michael Shollaj <55102646+Michael-Shollaj@users.noreply.github.com>
2022-05-19 20:43:50 +0200

    Initial Commit

---
+++ Assets/Scripts/ShowScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowScore : MonoBehaviour {
+
+	public Text scoreText;
+	public Text highScore;
+
+	// Use this for initialization
+	void Start () {
+		highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+		scoreText.text = "Score: " + Score.score;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		
+	}
+}
